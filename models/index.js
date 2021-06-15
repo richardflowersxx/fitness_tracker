8be5b15b7eb2067852b0f7e27e9ae3f2cf177bbc
Ricardo Flores <richard.flowersxx@gmail.com>
2021-06-15 19:31:32 -0400

    first commit

---
+++ models/index.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const trackerSchema = new Schema({
+    excercises: [{
+      {
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter a exercise type"
+        },
+        name: {
+            type: String,
+            required: "Enter a excercise name"
+        },
+        duration: {
+            type: Number
+
+        },
+        weight:{
+            type: Number
+
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        },
+
+    }
+    ],
+
+    day: {
+    type: Date,
+    default: Date.now
+}
+});
+
+const Tracker = mongoose.model("Tracker", trackerSchema);
+
+module.exports = Tracker;
